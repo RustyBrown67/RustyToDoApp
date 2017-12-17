95dc47aab76095979896e42f8a8d23061525192d
Russell Brown <russell.brown67@googlemail.com>
2017-12-17 10:10:43 +0000

    Initial Commit

---
+++ RustyToDoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RustyToDoApp
+//
+//  Created by Russell Brown on 17/12/2017.
+//  Copyright © 2017 Russell Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
