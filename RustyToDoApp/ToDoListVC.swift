f3a79be7b98a6251c59223f048699e98b7f8e8e4
Russell Brown <russell.brown67@googlemail.com>
2017-12-17 13:04:38 +0000

    To Do list complete with datasource and delegate methods

---
+++ RustyToDoApp/ToDoListVC.swift
@@ -0,0 +1,52 @@
+//
+//  ToDoListVC.swift
+//  RustyToDoApp
+//
+//  Created by Russell Brown on 17/12/2017.
+//  Copyright © 2017 Russell Brown. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListVC: UITableViewController {
+
+    let itemArray = ["Hemswell Snag List", "Costing Engineers", "Shopping List"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //MARK - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        //Place checkmark or remove checkmark when selecting item
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    
+
+
+}
+
